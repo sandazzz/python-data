0a7a01ed9c8b532080888c8c4f33a8d2f5cb9509
sandazzz <rsandarisoa@gmail.com>
2024-12-19 11:46:55 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
-work_time = pd.read_csv('data/temps-de-travail-annuel-depuis-1851.csv')
-frequentation = pd.read_csv("data/frequentation-gares.csv")
+def get_clean_work_time(work_time_path):
+    return (
+        pd.read_csv(work_time_path, sep=';', encoding='utf-8')
+        .drop_duplicates()
+        .fillna({'Temps annuel de travail (SNCF)': 0,
+                 'Temps annuel de travail (France)': 0,
+                 'Commentaires': ''})
+        .astype({'Date': int,
+                 'Temps annuel de travail (SNCF)': int,
+                 'Temps annuel de travail (France)': int})
+        .assign(Commentaires=lambda x: x['Commentaires'].str.strip())
+    )
 
-print(frequentation)
+work_time = get_clean_work_time('data/temps-de-travail-annuel-depuis-1851.csv')
+frequentation = pd.read_csv("data/frequentation-gares.csv", sep=";")
 
+print(work_time)
